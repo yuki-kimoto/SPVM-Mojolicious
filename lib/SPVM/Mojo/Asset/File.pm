c17c32151cf025a592531fbe4969f9283b7e1a3d
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-08-02 09:46:09 +0900

    Update Changes.

--- lib/SPVM/Mojo/Asset/File.pm
+++ lib/SPVM/Mojo/Asset/File.pm
@@ -15,6 +15,20 @@ Mojo::Asset::File class in L<SPVM> is a file storage backend for HTTP content.
 =head1 Usage
 
   use Mojo::Asset::File;
+  
+  # Temporary file
+  my $file = Mojo::Asset::File->new;
+  $file->add_chunk("foo bar baz");
+  if ($file->contains("bar") >= 0) {
+    say "File contains \"bar\"" ;
+  }
+  say $file->slurp;
+  
+  # Existing file
+  my $file = Mojo::Asset::File->new;
+  $file->set_path("/home/sri/foo.txt");
+  $file->move_to("/yada.txt");
+  say $file->slurp;
 
 =head1 Fields
 
@@ -55,57 +69,57 @@ Creates a new L<Mojo::Asset::File|SPVM::Mojo::Asset::File> object, and returns i
 
 =head2 add_chunk
 
-C<method add_chunk : L<Mojo::Asset::File|SPVM::Mojo::Asset::File> ($chunk : string);
+C<method add_chunk : L<Mojo::Asset::File|SPVM::Mojo::Asset::File> ($chunk : string);>
 
 Add chunk of data.
 
 =head2 contains
 
-C<method contains : int ($string : string);
+C<method contains : int ($string : string);>
 
 Check if asset contains a specific string.
 
 =head2 get_chunk
 
-C<method get_chunk : string ($offset : long, $max : int = -1);
+C<method get_chunk : string ($offset : long, $max : int = -1);>
 
 Get chunk of data starting from a specific position, defaults to a maximum chunk size of C<131072> bytes (128KiB).
 
 =head2 is_file
 
-C<method is_file : int ();
+C<method is_file : int ();>
 
-True, this is a L<Mojo::Asset::File> object.
+True, this is a L<Mojo::Asset::File|SPVM::Mojo::Asset::File> object.
 
 =head2 move_to
 
-C<method move_to : void ($file : string);
+C<method move_to : void ($file : string);>
 
 Move asset data into a specific file and disable L</"cleanup">.
 
 =head2 mtime
 
-C<method mtime : long ();
+C<method mtime : long ();>
 
 Modification time of asset.
 
 =head2 size
 
-C<method size : long ();
+C<method size : long ();>
 
 Size of asset data in bytes.
 
 =head2 slurp
 
-C<method slurp : string ();
+C<method slurp : string ();>
 
 Read all asset data at once.
 
 =head2 to_file
 
-C<method to_file : L<Mojo::Asset::File|SPVM::Mojo::Asset::File> ();
+C<method to_file : L<Mojo::Asset::File|SPVM::Mojo::Asset::File> ();>
 
-Does nothing but return the invocant, since we already have a L<Mojo::Asset::File> object.
+Does nothing but return the invocant, since we already have a L<Mojo::Asset::File|SPVM::Mojo::Asset::File> object.
 
 =head1 See Also
 
