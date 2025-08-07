1e4d9f3f1bdcd414a8fa0404d3a417c4cccb11cc
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-08-07 09:18:27 +0900

    Improve Time class.

--- lib/SPVM/Mojo/Date.pm
+++ lib/SPVM/Mojo/Date.pm
@@ -6,35 +6,33 @@ package SPVM::Mojo::Date;
 
 =head1 Name
 
-SPVM::Mojo::Date - Short Description
+SPVM::Mojo::Date - HTTP date
 
 =head1 Description
 
-Mojo::Date class in L<SPVM> has methods to do someting.
+Mojo::Date class in L<SPVM> implements HTTP date and time functions, based on L<RFC 7230|https://tools.ietf.org/html/rfc7230>, L<RFC
+7231|https://tools.ietf.org/html/rfc7231> and L<RFC 3339|https://tools.ietf.org/html/rfc3339>.
 
 =head1 Usage
 
   use Mojo::Date;
+  use Sys;
 
-=head1 Details
+  # Parse
+  my $date = Mojo::Date->new("Sun, 06 Nov 1994 08:49:37 GMT");
+  say $date->epoch;
 
-
-
-=head1 Super Class
-
-
-
-=head1 Interfaces
-
-
-
-=head1 Enumerations
-
-
+  # Build
+  my $date = Mojo::Date->new(Sys->time + 60);
+  say $date->to_string;
 
 =head1 Fields
 
+=head2 epoch
 
+C<has epoch : rw long;>
+
+Epoch seconds, defaults to the current time.
 
 =head1 Class Methods
 
@@ -46,16 +44,11 @@ Mojo::Date class in L<SPVM> has methods to do someting.
 
 =head1 See Also
 
+=over 2
 
+=item * L<SPVM::Mojolicious>
 
-
-=head1 Repository
-
-
-
-=head1 Author
-
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+=back
 
 =head1 Copyright & License
 
