71f216a3c6a5f47a9ae61b6b3e5a0cf9e885af04
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-08-11 10:12:56 +0900

    Improve Mojo::File.

--- lib/SPVM/Mojo/File.pm
+++ lib/SPVM/Mojo/File.pm
@@ -6,15 +6,30 @@ package SPVM::Mojo::File;
 
 =head1 Name
 
-SPVM::Mojo::File - Short Description
+SPVM::Mojo::File - File system paths
 
 =head1 Description
 
-Mojo::File class in L<SPVM> has methods to do someting.
+Mojo::File class in L<SPVM> is a scalar-based container for file system paths that provides a friendly API for dealing with different
+operating systems.
 
 =head1 Usage
 
   use Mojo::File;
+
+  # Portably deal with file system paths
+  my $path = Mojo::File->new("/home/sri/.vimrc");
+  say $path->slurp;
+  say $path->dirname->to_string;
+  say $path->basename;
+  say $path->extname;
+  say $path->sibling(".bashrc")->to_string;
+
+  # Use the alternative constructor
+  my $path = Mojo::File->path("/tmp/foo/bar")
+  $path->make_path;
+  $path->child("test.txt")
+  $path->spew("Hello Mojo!");
 
 =head1 Details
 
