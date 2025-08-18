6361afed3a68d2ab8f7a8e21c049308ae3ca298c
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-08-18 15:17:13 +0900

    Improve Mojo::Content.

--- lib/SPVM/Mojo/Content.pm
+++ lib/SPVM/Mojo/Content.pm
@@ -30,7 +30,49 @@ L<Mojo::EventEmitter|SPVM::Mojo::EventEmitter>
 
 =head1 Fields
 
+=head2 auto_decompress
 
+C<has auto_decompress : rw byte;>
+
+Decompress content automatically if L</"is_compressed"> is true.
+
+=head2 auto_relax
+
+C<has auto_relax : rw byte;>
+
+Try to detect when relaxed parsing is necessary.
+
+=head2 headers
+
+C<has headers : rw Mojo::Headers;>
+
+Content headers, defaults to a L<Mojo::Headers|SPVM::Mojo::Headers> object.
+
+=head2 max_buffer_size
+
+C<has max_buffer_size : rw int;>
+
+Maximum size in bytes of buffer for content parser, defaults to the value of the C<SPVM_MOJO_MAX_BUFFER_SIZE> environment
+variable or C<262144> (256KiB).
+
+=head2 max_leftover_size
+
+C<has max_leftover_size : rw int;>
+
+Maximum size in bytes of buffer for pipelined HTTP requests, defaults to the value of the C<SPVM_MOJO_MAX_LEFTOVER_SIZE>
+environment variable or C<262144> (256KiB).
+
+=head2 relaxed
+
+C<has relaxed : rw byte;>
+
+Activate relaxed parsing for responses that are terminated with a connection close.
+
+=head2 skip_body
+
+C<has skip_body : rw byte;>
+
+Skip body parsing and finish after headers.
 
 =head1 Class Methods
 
@@ -42,20 +84,16 @@ L<Mojo::EventEmitter|SPVM::Mojo::EventEmitter>
 
 =head1 See Also
 
+=over 2
 
+=item * L<Mojo::Headers|SPVM::Mojo::Headers>
 
+=item * L<SPVM::Mojolicious>
 
-=head1 Repository
-
-
-
-=head1 Author
-
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+=back
 
 =head1 Copyright & License
 
 Copyright (c) 2025 Yuki Kimoto
 
 MIT License
-
