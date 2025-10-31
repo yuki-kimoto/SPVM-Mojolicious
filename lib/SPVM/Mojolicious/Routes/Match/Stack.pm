a0afe99aaadc31223fff32011e149fc1ff8bedf4
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-10-31 09:43:16 +0900

    Add Mojolicious::Routes::Match::Stack class.

---
+++ lib/SPVM/Mojolicious/Routes/Match/Stack.pm
@@ -0,0 +1,71 @@
+package SPVM::Mojolicious::Routes::Match::Stack;
+
+
+
+1;
+
+=encoding utf8
+
+=head1 Name
+
+SPVM::Mojolicious::Routes::Match::Stack - Short Description
+
+=head1 Description
+
+Mojolicious::Routes::Match::Stack class in L<SPVM> has methods to do someting.
+
+=head1 Usage
+
+  use Mojolicious::Routes::Match::Stack;
+
+=head1 Details
+
+
+
+=head1 Super Class
+
+
+
+=head1 Interfaces
+
+
+
+=head1 Enumerations
+
+
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Well Known Child Classes
+
+
+
+=head1 See Also
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2025 Yuki Kimoto
+
+MIT License
+
